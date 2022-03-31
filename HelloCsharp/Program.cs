2d6826aeb6575d957b627f90b53c55a46c85935c
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-31 15:03:36 +0200

    Aggiungere i file di progetto.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Benvenuto!");
+
+Console.WriteLine("I mesi dell'anno sono");
+
+Console.WriteLine("Gennaio");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo"); 
+Console.WriteLine("Aprile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugno");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("Novembre");
+Console.WriteLine("Dicembre");
+
+var stringa = Console.ReadLine(); 
+Console.Write("Inbserisci il tuo mese preferito: " + stringa);
+
+Console.WriteLine("Arrivederci");
